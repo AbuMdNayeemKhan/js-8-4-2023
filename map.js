11f413b92934fb260fbfd76b66885c959adbece8
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-04-09 12:11:38 +0600

    learing javascript

--- map.js
+++ map.js
@@ -1,10 +1,21 @@
-let myArray = ['nayeem', 'zioun', 'tishat', 'rafi', 'rohan'];
-let empArray = [];
+// let myArray = ['nayeem', 'zioun', 'tishat', 'rafi', 'rohan'];
+// let empArray = [];
 
-myArray.forEach(showData);
+// myArray.forEach(showData);
 
-function showData(x){
-    empArray.push(x);
-};
+// function showData(x){
+//     empArray.push(x);
+// };
 
-console.log(empArray);
+// console.log(empArray);
+
+//==========>JS MAPING
+
+// let myArray = ['nayeem', 'zioun', 'tishat', 'rafi', 'rohan'];
+// let empArray = myArray.map(myFun);
+
+// function myFun(x){
+//     return x;
+// }
+
+// console.log(empArray);
